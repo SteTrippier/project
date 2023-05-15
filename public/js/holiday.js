710bf473081a02eb04b94aa10e2aa02a8631f0b3
Steven Trippier <steventrippier@hotmail.co.uk>
2023-05-15 23:22:40 +0100

    updated some of the holiday functionality, still not working properly

--- public/js/holiday.js
+++ public/js/holiday.js
@@ -1,111 +1,73 @@
-// auto email confirmation
 
-var nodemailer = require('nodemailer');
-var senderEmail = "emailadress@googlemail.com";
-var senderPass = "";
-var receiverEmail = "";
+function holidayInit(){
+  console.log("holiday init() called");
+  //test data
 
-var emailTitle ={
-    success: "Holiday booking confirmed!",
-    fail: "Holiday booking failed"
-};
-var emailBody = {
-    success: "Your holiday has been successfully booked for startDate until endDate",
-    fail: "Your holiday booking for startDate until endDate could not be processed due to reason"
+  bookHoliday(new Date("2023-07-28"), new Date("2023-07-29"));
 }
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: senderEmail,
-    pass: senderPass
-  }
-});
+async function getHolidays() {
+  const response = await fetch("/getholidays");
+  const holidays = await response.json();
+  console.log(holidays);
+  return holidays;
 
-var mailSuccessOptions = {
-  from: senderEmail,
-  to: receiverEmail,
-  subject: emailTitle.sucesss,
-  text: emailBody.success
 }
-
-var mailFailOptions = {
-    from: senderEmail,
-    to: receiverEmail,
-    subject: emailTitle.fail,
-    text: emailBody.fail
-}
-
-
-
-
-
-
 
 // checks if there will be a holiday overlap
-function isHolidayOverlap(startDate, endDate, bookedHolidays) {
+async function isHolidayOverlap(startDatei, endDatei) {
+
   // Iterate through each booked holiday
+  var bookedHolidays = await getHolidays();
+  var startDate = new Date(startDatei);
+  var endDate = new Date(endDatei);
+  
+  if(bookedHolidays.length<1){
+    console.log("no holidays");
+    return false;
+  }
   for (let i = 0; i < bookedHolidays.length; i++) {
-    const holidayStart = new Date(bookedHolidays[i].start);
-    const holidayEnd = new Date(bookedHolidays[i].end);
-    // Check if the requested date range overlaps with the current booked holiday
-    if (startDate <= holidayEnd && endDate >= holidayStart) {
-       return true; // Overlap found, return true
+    var holidayStart = new Date(bookedHolidays[i].start_date);
+    var holidayEnd = new Date(bookedHolidays[i].end_date);
+    console.log("holidayStart: " + holidayStart);
+    console.log("holidayEnd: " + holidayEnd);
+    // Check if and of the dates in the input range are between the start and end of any other booked holiday
+    if ((startDate.getTime() <= holidayEnd.getTime()) && (endDate.getTime() >= holidayStart.getTime())) {
+      return true;
     }
   }
-  // No overlap found, so book the holiday and return false
-  bookHoliday(startDate , endDate );
   return false;
 }
 
-async function bookHoliday (startDate, endDate ){
-    // code to post holiday to database and refresh the page;
-    //todo: get comment from form
-    //todo: get employee id from session
-    const booked = await fetch("/bookholiday",
-    {
-        method: post,
+
+
+// checks if the requested holiday is valid
+async function bookHoliday (startDate, endDate){
+  //todo: get comment from form
+  //todo: get employee id from session
+  employeeId = 0001;
+  comment = "test comment";
+  if (isHolidayOverlap(startDate, endDate)) {
+    //alert("Holiday overlaps with another holiday");
+    await fetch("/bookholiday",{
+      method: 'POST',
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({stat: "fail"})
+    });
+    return;
+  }
+  else{
+    const booked = await fetch("/bookholiday",{
+        method: 'POST',
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({
-            startDate: startDate,
-            endDate: endDate,
-            employeeId: employeeId,
-            comment: comment
-        })
-    }
-    );
-
-    if (booked.status == 200){
-        alert("Holiday booked successfully");
-        transporter.sendMail(mailSuccessOptions, function(error, info){
-          if (error) {
-            console.log(error);
-          } else {
-            console.log('Email sent: ' + info.response);
-          }
-        });
-    }
-    else{
-        alert("Holiday booking failed");
-        transporter.sendMail(mailFailOptions, function(error, info){
-          if (error) {
-            console.log(error);
-          } else {
-            console.log('Email sent: ' + info.response);
-          }
-        });
-    } 
-
-    location.reload();
-}
-
-
-
-
-
-
-
-
-
+        body: JSON.stringify({stat: "success", startDate: startDate,endDate: endDate,employeeId: employeeId,comment: comment})
+    });
+    console.log("booked holiday, no overlap")
+    console.log(booked);
+  }
+  //location.reload();
+} 
--- server.js
+++ server.js
@@ -1,22 +1,29 @@
+require('dotenv').config({ path: __dirname + '/projectVariables.env' })
 const express = require("express");
 const app = express();
-const port = 1093;
+const port = process.env.WEB_LISTEN_PORT || 1093;
 const login = require("./public/js/harrisonsLogin.js");
 const bodyParser = require('body-parser')
 const { Pool } = require('pg');
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 var users = [];
-
+const tls = require('tls');
+// Create a custom TLS configuration to trust self-signed certificates
+const tlsConfig = {
+  rejectUnauthorized: false // Set to false to trust self-signed certificates
+};
+// Set the NODE_TLS_REJECT_UNAUTHORIZED environment variable to "0"
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 // Create a connection pool to the PostgreSQL database
 // Todo repace hardcode values with environment variables
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'Stein21244!',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
 });
 
 app.set('views', __dirname + '/views');
@@ -30,10 +37,57 @@ app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// auto email confirmation
+var nodemailer = require('nodemailer');
+var senderEmail = process.env.EMAIL_ADD;
+var senderPass = process.env.EMAIL_PASS;
+var receiverEmail = process.env.TEST_EMAIL;
+
+var emailTitle ={
+    success: "Holiday booking confirmed!",
+    fail: "Holiday booking failed"
+};
+var emailBody = {
+    success: "Your holiday has been successfully booked for startDate until endDate",
+    fail: "Your holiday booking for startDate until endDate could not be processed due to reason"
+}
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: senderEmail,
+    pass: senderPass
+  },
+  tls: tlsConfig
+});
+
+var mailSuccessOptions = {
+  from: senderEmail,
+  to: receiverEmail,
+  subject: emailTitle.success,
+  text: emailBody.success
+}
+
+var mailFailOptions = {
+    from: senderEmail,
+    to: receiverEmail,
+    subject: emailTitle.fail,
+    text: emailBody.fail
+}
+
+function holidayFailEmail(){
+  console.log("holidayFailEmail");
+  transporter.sendMail(mailFailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+} 
 
 
-
-app.listen(port, () => console.log("Servers has started on port: " + port));
+app.listen(port, () => console.log("Servers has started on port: " + process.env.WEB_LISTEN_PORT));
 
 // Check if the user is logged in before allowing access to the page.
 function checkAuth(req, res, next) {
@@ -49,6 +103,10 @@ app.get("/", (req, res) => {
 });
 
 
+
+
+
+// NOTICES API
 // Define a route to handle GET requests to retrieve the notices
 app.get('/notices', async (req, res) => {
   console.log ('GET /notices called');
@@ -64,8 +122,6 @@ app.get('/notices', async (req, res) => {
     res.status(500).json({ error: 'Error retrieving notices' });
   }
 });
-
-
 // Define a route to handle POST requests to add a new notice
 app.post('/notices', async (req, res) => {
   console.log ('POST /notices called');
@@ -88,6 +144,9 @@ app.post('/notices', async (req, res) => {
 
 
 
+
+
+// LOG IN AND SIGN UP API
 //send login page
 app.get('/login', (req, res) => {
   res.sendFile(__dirname + "/login.html");
@@ -151,17 +210,57 @@ app.post("/deleteholiday", async (req, res) => {
     console.error('Error deleting holiday:', error);
     res.status(500).json({ error: 'Error deleting holiday' });
   }
-}
-);
+});
+
 app.post("/bookholiday", async (req, res) => {
+  
   console.log(req.body);
-  try{
-    const { startDate, endDate, employeeId } = req.body;
-    await pool.query('INSERT INTO holidays (startdate, enddate, employeeid) VALUES ($1, $2, $3)', [startDate, endDate, employeeId]);
-    res.json({ message: 'Holiday booked successfully' });
+  if (req.body.stat == "success"){
+    try{
+      const { startDate, endDate, employeeId, comment } = req.body;
+      await pool.query('INSERT INTO holidays (start_date, end_date, employee_id, comment) VALUES ($1, $2, $3, $4)', [startDate, endDate, employeeId, comment]);
+      console.log("success, you booked the holiday.")
+      holidaySuccessEmail();
+      res.json({ message: 'Holiday booked' });
+    }
+    catch (error) {
+      holidayFailEmail();
+      console.error('Error booking holiday:', error);
+      res.status(500).json({ error: 'Error booking holiday' });
+    }
   }
-  catch (error) {
-    console.error('Error booking holiday:', error);
-    res.status(500).json({ error: 'Error booking holiday' });
+  else if (req.body.stat == "fail"){
+    console.log("failed to book the holiday.")
+    holidayFailEmail();
+  }
+  else{
+    console.log("Something else went wrong.")
   }
 });
+
+function holidaySuccessEmail(){
+    console.log("holidaySuccessEmail");
+    transporter.sendMail(mailSuccessOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+}
+
+
+app.get("/getholidays", async (req, res) => {
+
+  
+  try{
+    const result = await pool.query('SELECT * FROM holidays');
+    console.log(result.rows);
+    res.json(result.rows);
+  }
+  catch (error) {
+    console.error('Error getting holidays:', error);
+    res.status(500).json({ error: 'Error getting holidays' });
+  }
+});
+
